15d90efe06a7af03f23de19aa9dea55e91a5fe90
duyguhz <duyghuhh@code.edu.az>
2022-09-11 10:56:59 +0400

    hw

---
+++ homework-late-1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace homework_late_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
